32176ec3d52aa872929a5cf7b8653986ef8be9a9
CassioAA <cassioalmeidaccti@gmail.com>
2025-01-18 14:59:51 -0300

    feat: Add quiz's page and interactions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,61 @@
 import "../App.css";
+import { Questions } from "../helpers/Questions";
+import {useState} from "react";
 
 function Quiz() {
-	return <div>Starting</div>;
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [optionChosen, setOptionChosen] = useState("");
+
+	function chooseOption(option) {
+		setOptionChosen(option);
+	}
+
+	function nextQuestion() {
+		if (Questions[currentQuestion].answer == optionChosen) {
+			alert("correct");
+		} else {
+			alert("incorrect");
+		}
+
+		setCurrentQuestion(currentQuestion + 1);
+	}
+
+	return (
+		<div>
+			<div className="Quiz">
+				<h1>{Questions[currentQuestion].prompt}</h1>
+				<button
+					onClick={() => {
+						chooseOption("optionA");
+					}}
+				>
+					{Questions[currentQuestion].optionA}
+				</button>
+				<button
+					onClick={() => {
+						chooseOption("optionB");
+					}}
+				>
+					{Questions[currentQuestion].optionB}
+				</button>
+				<button
+					onClick={() => {
+						chooseOption("optionC");
+					}}
+				>
+					{Questions[currentQuestion].optionC}
+				</button>
+				<button
+					onClick={() => {
+						chooseOption("optionD");
+					}}
+				>
+					{Questions[currentQuestion].optionD}
+				</button>
+			</div>
+			<button onClick={nextQuestion}>Próxima</button>
+		</div>
+	);
 }
 
 export default Quiz;
---
+++ src/helpers/Questions.js
@@ -0,0 +1,18 @@
+export const Questions = [
+  {
+    prompt: "Qual dessas linguagens de alto nível é a mais próxima de Assembly?",
+    optionA: "Python",
+    optionB: "C",
+    optionC: "Javascript",
+    optionD: "Java",
+    answer: "optionB",
+  },
+  {
+    prompt: "Qual dessas opções não é um framework javascript?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Echo",
+    answer: "optionD",
+  },
+];
